d700744cada066f3c482a7b54d5777c079ce0253
Koziorus <87280018+Koziorus@users.noreply.github.com>
2023-03-28 22:42:37 +0200

    final MN lab 3 update

--- MN/lab3/main_lab3.m
+++ MN/lab3/main_lab3.m
@@ -27,9 +27,9 @@ if issparse(M) == false
 end
 
 b = linspace((1-d)/N, (1-d)/N, N)';
+
 %% Zadanie C
 r = M\b;
-
 
 %% Zadanie D
 %------------------
@@ -38,7 +38,6 @@ clear all
 close all
 
 N = [500, 1000, 3000, 6000, 12000];
-
 
 for i = 1:5
     [M, b] = prepare_Mb(N(i), 10);
@@ -60,7 +59,6 @@ N = [500, 1000, 3000, 6000, 12000];
 residuals = zeros(1,1000);
 
 for i = 1:5
-    display(N(i))
     [M, b] = prepare_Mb(N(i), 10);
     L = tril(M,-1);
     U = triu(M,1);
@@ -97,9 +95,95 @@ semilogy(residuals); % pod 2 indeksem jest liczba iteracji dla N = 1000
 
 %% Zadanie F
 
+clc
+clear all
+close all
+
+N = [500, 1000, 3000, 6000, 12000];
+residuals = zeros(1,1000);
+
+for i = 1:5
+    [M, b] = prepare_Mb(N(i), 10);
+    L = tril(M,-1);
+    U = triu(M,1);
+    D = diag(diag(M));
+    r = ones(N(i), 1);
+    X = -(D+L)\speye(N(i),N(i));
+    Y = (D+L)\b;
+    r = X * (U*r) + Y;
+    res = M*r - b;
+
+    iterations(i) = 0;
+    
+    tic
+    % obliczenia start
+    while norm(res) > 10^(-14)
+        r = X * (U*r) + Y;
+
+        res = M*r - b;
+        
+        iterations(i) = iterations(i) + 1;
+
+        if N(i) == 1000
+            residuals(iterations(i)) = norm(res);
+        end
+    end
+    % obliczenia stop
+    czas_GaussSeid(i) = toc;
+end
+plot(N, czas_GaussSeid)
+figure
+plot(N, iterations)
+figure
+semilogy(residuals); % pod 2 indeksem jest liczba iteracji dla N = 1000
 
 %% Zadanie G
 
+% bezposrednia...
+data = matfile("Dane_Filtr_Dielektryczny_lab3_MN.mat");
+M = data.M;
+b = data.b;
+N = 20000;
 
+r =  M\b;
+% ...
 
+% jacobi ...
+L = tril(M,-1);
+U = triu(M,1);
+D = diag(diag(M));
+r = ones(N, 1);
+X = -D\(L+U);
+Y = D\b;
+r = X * r + Y;
+res = M*r - b;
+
+tic
+% obliczenia start
+while norm(res) > 10^(-14)
+    r = X * r + Y;
+    res = M*r - b;
+end
+czas_Jacobi = toc;
+display(czas_Jacobi)
+% ...
+
+% gauss_seid ...
+L = tril(M,-1);
+U = triu(M,1);
+D = diag(diag(M));
+r = ones(N, 1);
+X = -(D+L)\speye(N,N);
+Y = (D+L)\b;
+r = X * (U*r) + Y;
+res = M*r - b;
+
+tic
+% obliczenia start
+while norm(res) > 10^(-14)
+    r = X * (U*r) + Y;
+    res = M*r - b;
+end
+czas_GaussSeid = toc;
+display(czas_GaussSeid)
 
